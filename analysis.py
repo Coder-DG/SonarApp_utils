9a6f24bd54ccaa529f77bb44fc45a50aaa0945fb
David Ginzbourg <davidginzbourg@gmail.com>
2019-08-30 12:01:48 -0700

    Remove bad cc trimming

--- analysis.py
+++ analysis.py
@@ -30,7 +30,6 @@ def show_cross_correlation(number):
     with open(ccorrelation_file, 'r') as f:
         data = json.load(f)['data']
     y = list(float(n) for n in data)
-    y = y[:len(y) // 2]
     return get_graph_figure(y, ccorrelation_file)
 
 
