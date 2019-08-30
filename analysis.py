91c3ce7aea02d19f4fc97ba791bbaeb3819db6ec
David Ginzbourg <davidginzbourg@gmail.com>
2019-08-29 17:12:23 -0700

    Trim recording data

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 CHIRP_DURATION = 0.01
 SAMPLE_RATE = 44100
@@ -7,32 +8,31 @@ SAMPLE_RATE = 44100
 
 def show_cross_correlation(number):
     ccorrelation_file = 'samples/cross_correlation_of_' + str(number)
-    return get_graph_figure(ccorrelation_file)
+    with open(ccorrelation_file, 'r') as f:
+        data = json.load(f)['data']
+    y = list(float(n) for n in data)
+    return get_graph_figure(y, ccorrelation_file)
 
 
 def show_recording(number):
     recording_file = 'samples/recording_of_' + str(number)
-    return get_graph_figure(recording_file)
+    with open(recording_file, 'r') as f:
+        data = json.load(f)['data']
+    y = list(int(n) for n in data)
+    argmax = np.argmax(y)
+    start_y = max(np.floor(argmax - CHIRP_DURATION * SAMPLE_RATE * 0.5), 0)
+    y = y[start_y:]
+    return get_graph_figure(y, recording_file)
 
 
-def get_graph_figure(file_name):
+def get_graph_figure(y, title):
+    x = list(range(len(y)))
+
     plt.grid(True, axis='y')
     fig = plt.figure()
     ax = fig.add_axes([0, 0, 1, 1])
-
-    with open(file_name, 'r') as f:
-        data = json.load(f)
-        y = list(int(n) for n in data['data'])
-    start_y = 0
-    for i in range(len(y)):
-        if y[i] == 0:
-            start_y += 1
-        else:
-            break
-    y = y[start_y:]
-    x = list(range(len(y)))
-    ax.plot(x, y)
     ax.set_xlabel('Sample #')
     ax.set_ylabel('Amplitude')
-    ax.set_title(file_name)
+    ax.set_title(title)
+    ax.plot(x, y)
     return fig
